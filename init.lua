3e38f898042b77a7daf38237f120efbef37c4299
Tamego <oitrosutuik@gmail.com>
2024-08-21 16:43:03 +0900

    add: コマンドラインをすぐ削除するように

--- init.lua
+++ init.lua
@@ -208,6 +208,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('CmdlineLeave', {
+  group = vim.api.nvim_create_augroup('kickstart-clear_cmdline', { clear = true }),
+  callback = function()
+    print ' '
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
